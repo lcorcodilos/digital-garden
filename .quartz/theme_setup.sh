132b5b4121acedf7a7bf9fceafb8b08d102097f7
Lucas Corcodilos <corcodilos.lucas@gmail.com>
2025-07-20 17:43:58 -0400

    Use vanilla echo

--- .quartz/theme_setup.sh
+++ .quartz/theme_setup.sh
@@ -1,8 +1,8 @@
 if grep -q '^@use "./themes";' "quartz/styles/custom.scss"; then
   # Import already present in custom.scss
-  echo_warn "Theme import line already present in custom.scss. Skipping..."
+  echo "Theme import line already present in custom.scss. Skipping..."
 else
   # Add `@use "./themes";` import to custom.scss
   sed -ir 's#@use "./base.scss";#@use "./base.scss";\n@use "./themes";#' "quartz/styles/custom.scss"
-  echo_info "Added import line to custom.scss..."
+  echo "Added import line to custom.scss..."
 fi
